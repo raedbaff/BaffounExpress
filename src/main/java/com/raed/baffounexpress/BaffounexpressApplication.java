3fd2b51aa81a57cc377cd946a2dc29d375e1f5db
raedbaff <raed.baffoun@polytechnicien.tn>
2025-11-25 21:04:41 +0100

    initial commit

---
+++ src/main/java/com/raed/baffounexpress/BaffounexpressApplication.java
@@ -0,0 +1,13 @@
+package com.raed.baffounexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaffounexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaffounexpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raed/baffounexpress/BaffounexpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raed.baffounexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaffounexpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
